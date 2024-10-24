7403197c41200c09d16005f6635b9481a90b6b6c
Murage <25152892+0xMurage@users.noreply.github.com>
2024-10-24 05:47:43 +0300

    ci: remove flag preventing push of changelog and updated version if applicable

--- packages/release-it-config/.release-it.cjs
+++ packages/release-it-config/.release-it.cjs
@@ -6,12 +6,11 @@ module.exports = {
         }
     },
     git: {
-        tag: false,
-        commitsPath: '.',
-        requireCommits: true,
-        requireCommitsFail: false,
+        tag: false, // We want the last version to be derived from the package.json version field.
+        commitsPath: '.', // Only consider commits in this folder and not the whole repository.
+        requireCommits: true, // Do not do anything with git if there are no commits.
+        requireCommitsFail: false, // Do not fail if there are no commits.
         commitMessage: "chore(release): released version v${version} [no ci]",
-        push: false
     },
     npm: {
         publish: false
